2f372010f3386f33a79b2214fc538e5cce3612cf
SHAIK SIRAJ <149253815+shaiksiraj07@users.noreply.github.com>
2026-02-11 11:24:42 +0530

    Remove commented code and clean up formatting

--- 27_Remove-element.java
+++ 27_Remove-element.java
@@ -5,7 +5,6 @@ Consider the number of elements in nums which are not equal to val be k, to get 
 Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
 Return k.
 Custom Judge:
-
 The judge will test your solution with the following code:
 
 int[] nums = [...]; // Input array
@@ -21,9 +20,6 @@ for (int i = 0; i < actualLength; i++) {
     assert nums[i] == expectedNums[i];
 }
 If all assertions pass, then your solution will be accepted.
-
- 
-
 Example 1:
 
 Input: nums = [3,2,2,3], val = 3
@@ -37,16 +33,15 @@ Output: 5, nums = [0,1,4,0,3,_,_,_]
 Explanation: Your function should return k = 5, with the first five elements of nums containing 0, 0, 1, 3, and 4.
 Note that the five elements can be returned in any order.
 It does not matter what you leave beyond the returned k (hence they are underscores).
- 
 
 Constraints:
 
 0 <= nums.length <= 100
 0 <= nums[i] <= 50
-0 <= val <= 100*/
+0 <= val <= 100
 
 
-
+==================================================================*/
 
 class Solution {
     public int removeElement(int[] nums, int val) {
@@ -59,4 +54,5 @@ class Solution {
      }
     return a;
     }
+
 }
