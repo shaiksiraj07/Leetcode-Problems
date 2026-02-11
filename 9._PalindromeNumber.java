d0cdcab352a41860b0b47f9e2f0d4a471a9e7c05
SHAIK SIRAJ <149253815+shaiksiraj07@users.noreply.github.com>
2026-02-11 11:26:12 +0530

    Update 9._PalindromeNumber.java
    Updated comments and formatting for clarity.

--- 9._PalindromeNumber.java
+++ 9._PalindromeNumber.java
@@ -1,12 +1,6 @@
 /*9. Palindrome Number
-
-
 Given an integer x, return true if x is a palindrome, and false otherwise.
-
- 
-
 Example 1:
-
 Input: x = 121
 Output: true
 Explanation: 121 reads as 121 from left to right and from right to left.
@@ -21,12 +15,12 @@ Input: x = 10
 Output: false
 Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
  
-
 Constraints:
 
 -231 <= x <= 231 - 1
  
 --------------------------------------------------------------------------------------------- */
+
 class Solution {
     public boolean isPalindrome(int x) {
         int temp=x,rev=0; 
