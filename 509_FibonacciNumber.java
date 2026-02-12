e144ef09d452226c8510a38baf80fd3664b31d27
SHAIK SIRAJ <149253815+shaiksiraj07@users.noreply.github.com>
2026-02-12 11:47:07 +0530

    update

--- 509_FibonacciNumber.java
+++ 509_FibonacciNumber.java
@@ -1,4 +1,5 @@
-/*509. Fibonacci Number
+/*
+509. Fibonacci Number
 
 The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
 F(0) = 0, F(1) = 1
@@ -19,7 +20,8 @@ Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
 Constraints:
 
 0 <= n <= 30
-==============================================================*/
+
+===============================================================================*/
 
 class Solution {
     public int fib(int n) {
