8f4fda35bd328feed6d5a7d3f8689a3b1e776887
SHAIK SIRAJ <149253815+shaiksiraj07@users.noreply.github.com>
2026-02-15 18:46:38 +0530

    Updated

--- 268_MissingNumber.java
+++ 268_MissingNumber.java
@@ -4,7 +4,6 @@ Given an array nums containing n distinct numbers in the range [0, n], return th
 Example 1:
 Input: nums = [3,0,1]
 Output: 2
-
 Explanation:
 n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
 Example 2:
@@ -17,6 +16,8 @@ Input: nums = [9,6,4,2,3,5,7,0,1]
 Output: 8
 Explanation:
 n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the range since it does not appear in nums. 
+
+
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- */
 
 class Solution {
