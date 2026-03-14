1f065f02fbdbd60ca77b8655787a3410dfe371f3
SHAIK SIRAJ <149253815+shaiksiraj07@users.noreply.github.com>
2026-03-14 12:03:27 +0530

    u

--- 50. Pow(x, n).java
+++ 50. Pow(x, n).java
@@ -1,5 +1,4 @@
 /*50. Pow(x, n)
-
 Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
 Example 1:
 Input: x = 2.00000, n = 10
@@ -12,7 +11,6 @@ Input: x = 2.00000, n = -2
 Output: 0.25000
 Explanation: 2-2 = 1/22 = 1/4 = 0.25
 Constraints:
-
 -100.0 < x < 100.0
 -231 <= n <= 231-1
 n is an integer.
